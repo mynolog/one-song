4ba411c5af0c87aca81edb75a3139706bae9528e
mynolog <minholee.log@gmail.com>
2025-04-30 00:17:27 +0900

    test: getBaseUrl 테스트 작성 및 유틸 함수 리팩토링(CSR 관련 코드 제거)

--- __tests__/lib/getter.test.ts
+++ __tests__/lib/getter.test.ts
@@ -1,4 +1,4 @@
-import { getRandomIndex } from '@/lib/getter'
+import { getRandomIndex, getBaseUrl } from '@/lib/getter'
 
 describe('getRamdomIndex', () => {
   test('0 이상 배열의 길이(length) 미만의 정수를 반환한다.', () => {
@@ -22,5 +22,30 @@ describe('getRamdomIndex', () => {
   test('배열의 길이(length)가 undefined일 경우 NaN이 된다.', () => {
     const result = getRandomIndex(undefined as any)
     expect(Number.isNaN(result)).toBe(true)
+  })
+})
+
+describe('getBaseUrl', () => {
+  afterEach(() => {
+    delete process.env.NEXT_PUBLIC_BASE_URL
+  })
+
+  test('환경 변수가 설정되어 있으면 url을 반환한다.', () => {
+    const url = 'http://localhost:9999'
+    process.env.NEXT_PUBLIC_BASE_URL = url
+    expect(getBaseUrl()).toBe(url)
+  })
+
+  test('환경 변수가 빈 문자열이라면 에러를 던진다.', () => {
+    process.env.NEXT_PUBLIC_BASE_URL = ''
+    expect(() => getBaseUrl()).toThrow(
+      '환경 변수 NEXT_PUBLIC_BASE_URL이 존재하지 않습니다.',
+    )
+  })
+
+  test('환경 변수가 존재하지 않는다면 에러를 던진다.', () => {
+    expect(() => getBaseUrl()).toThrow(
+      '환경 변수 NEXT_PUBLIC_BASE_URL이 존재하지 않습니다.',
+    )
   })
 })
