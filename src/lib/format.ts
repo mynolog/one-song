6ecdb153ccb5b6d2889ed4334a66945e958fa879
mynolog <minholee.log@gmail.com>
2025-04-29 12:31:31 +0900

    fix: formatTime 유틸 함수에서 0초 입력 시 정상 처리되도록 수정

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -11,7 +11,7 @@ export function formatDate(dateInput: string | Date) {
 }
 
 export function formatTime(seconds: number) {
-  if (!seconds) {
+  if (seconds === undefined || isNaN(seconds)) {
     throw new Error('유효하지 않은 입력입니다.')
   }
   return new Date(seconds * 1000).toISOString().substring(14, 19)
