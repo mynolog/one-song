5c7ce8d40beecb62b05aaac67ba657fa81ef6b77
mynolog <minholee.log@gmail.com>
2025-04-30 21:13:07 +0900

    feat: 국가별 동적 라우트 페이지 생성 및 홈 접근 시 랜덤한 국가별 페이지로 리다이렉트 처리

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,12 @@
-import type { SongsFeedResponse, Song } from '@/lib/songs'
-import RandomSongPicker from '@/components/home/RandomSongPicker'
 import { getRandomIndex } from '@/lib/getter'
 import { COUNTRIES } from '@/constants/rssQueryParams'
 import { getBaseUrl } from '@/lib/getter'
+import { redirect } from 'next/navigation'
 
 export default async function Home() {
   const randomCountryIndex = getRandomIndex(COUNTRIES.length)
   const countryCode = COUNTRIES[randomCountryIndex].code
   const baseURL = getBaseUrl()
 
-  const res = await fetch(`${baseURL}/api/songs?countryCode=${countryCode}`, {
-    cache: 'no-store',
-  })
-  const json: SongsFeedResponse = await res.json()
-  const songs: Song[] = json.results ?? []
-
-  return (
-    <div className="flex h-full w-full flex-col items-center justify-center">
-      <RandomSongPicker songs={songs} />
-    </div>
-  )
+  redirect(`${baseURL}/country/${countryCode}`)
 }
