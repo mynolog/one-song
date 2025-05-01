9cb9920cce409bc47616615c31e317ac7a8b9aed
mynolog <minholee.log@gmail.com>
2025-05-01 21:34:58 +0900

    refactor: 선택된 랜덤 곡이 없을 경우 셔플 버튼만 활성화 하도록 개선 및 오디오 플레이어 UI 개선

--- src/components/home/RandomSongCardSkeleton.tsx
+++ src/components/home/RandomSongCardSkeleton.tsx
@@ -1,11 +1,12 @@
-import { Heart, Music2, Music3 } from 'lucide-react'
+import { Heart, Music2 } from 'lucide-react'
 import { Button } from '../ui/button'
 import { formatDate } from '@/lib/format'
 
 export default function RandomSongCardSkeleton() {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex h-[300px] w-[300px] items-center justify-center rounded-md bg-[#f1f3f4]">
+    <div className="flex w-[50vw] flex-col gap-4 sm:w-75">
+      {/* <div className="relative -z-50 aspect-square w-full max-w-75 overflow-hidden rounded-md"></div> */}
+      <div className="-z-50 flex aspect-square w-full max-w-75 items-center justify-center rounded-md bg-[#f1f3f4]">
         <Music2 size={100} className="text-white" />
       </div>
       <div className="flex flex-col items-center justify-center gap-2">
@@ -17,15 +18,11 @@ export default function RandomSongCardSkeleton() {
             <Heart className="gray-400" />
           </Button>
         </div>
-        <span className="text-sm font-semibold text-gray-600">
+        <span className="text-xs font-semibold text-gray-600 sm:text-sm">
           추천 받기를 눌러 새로운 곡을 만나보세요!
         </span>
         <div className="text-muted-foreground text-xs">
           {formatDate(new Date())} 오늘의 노래
-        </div>
-        <div className="relative flex h-28 w-full items-center justify-center overflow-hidden rounded-md py-2 transition-all duration-200 ease-in-out hover:bg-[#f1f3f4]">
-          <Music3 />
-          <span className="text-sm">좋은 음악이 기다리고 있어요.</span>
         </div>
       </div>
     </div>
