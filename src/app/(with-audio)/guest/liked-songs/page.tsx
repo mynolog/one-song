c7175877b0133ef3c3a186f43fd78be8165601c0
mynolog <minholee.log@gmail.com>
2025-05-04 15:01:22 +0900

    refactor: LikedSong 페이지 UI 구조 개선(노래제목, 아티스트 통합 1행으로 통합)

--- src/app/(with-audio)/guest/liked-songs/page.tsx
+++ src/app/(with-audio)/guest/liked-songs/page.tsx
@@ -70,63 +70,64 @@ export default function GuestLikedSongsPage() {
   }
 
   return (
-    <div className="flex h-[90vh] w-[1200px] flex-col gap-4 px-4 pt-[180px]">
-      <Table>
-        <TableCaption className="caption-top pb-3">내가 찜한 노래</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>제목</TableHead>
-            <TableHead>아티스트</TableHead>
-            <TableHead>앨범</TableHead>
-            <TableHead>Apple Music</TableHead>
-            <TableHead>미리 듣기</TableHead>
-            <TableHead>찜 취소</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {likedSongs.map((song) => (
-            <TableRow key={song.id} className="w-full">
-              <TableCell className="w-[180px] max-w-[180px] truncate">
-                {song.name}
-              </TableCell>
-              <TableCell className="w-[160px] max-w-[160px] truncate">
-                {song.artistName}
-              </TableCell>
-              <TableCell className="w-[200px] max-w-[200px] truncate">
-                {song.collectionName}
-              </TableCell>
-              <TableCell className="w-[30px]">
-                <a
-                  href={song.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Apple Music에서 듣기"
-                >
-                  <AppleMusicIcon />
-                </a>
-              </TableCell>
-              <TableCell className="w-[30px]">
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => handleUpdatePickedSong(song)}
-                >
-                  <Headphones className="inline-block h-4 w-4" />
-                </Button>
-              </TableCell>
-              <TableCell className="w-[30px]">
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => handleRemoveLiked(song)}
-                >
-                  <Minus className="inline-block h-4 w-4" />
-                </Button>
-              </TableCell>
+    <div className="flex h-full min-h-[60vh] w-full max-w-[1200px] flex-col items-center gap-10 px-4">
+      <div className="min-h-[50vh] w-full">
+        <Table>
+          <TableCaption className="caption-top pb-3">내가 찜한 노래</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>노래</TableHead>
+              <TableHead className="hidden">앨범</TableHead>
+              <TableHead>Apple Music</TableHead>
+              <TableHead>미리 듣기</TableHead>
+              <TableHead>찜 취소</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {likedSongs.map((song) => (
+              <TableRow key={song.id} className="w-full text-xs">
+                <TableCell className="w-[400px] max-w-[400px] truncate">
+                  <div className="flex h-full w-full flex-col gap-1">
+                    <span className="font-semibold">{song.name}</span>
+                    <span>{song.artistName}</span>
+                  </div>
+                </TableCell>
+                <TableCell className="hidden w-[200px] max-w-[200px] truncate">
+                  {song.collectionName}
+                </TableCell>
+                <TableCell className="w-[30px]">
+                  <a
+                    href={song.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Apple Music에서 듣기"
+                  >
+                    <AppleMusicIcon />
+                  </a>
+                </TableCell>
+                <TableCell className="w-[30px]">
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={() => handleUpdatePickedSong(song)}
+                  >
+                    <Headphones className="inline-block h-4 w-4" />
+                  </Button>
+                </TableCell>
+                <TableCell className="w-[30px]">
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={() => handleRemoveLiked(song)}
+                  >
+                    <Minus className="inline-block h-4 w-4" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
       <div className="flex w-full items-center justify-center">
         <Button>
           <Link href={`/country/${countryCode}`}>재생 화면으로 돌아가기</Link>
