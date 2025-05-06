0aeb5c4f6cecb24db355ed824d6302c4140ef9dc
mynolog <minholee.log@gmail.com>
2025-05-06 11:35:06 +0900

    feat:auth.js 기본 설정 및 Provider 적용

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,10 +1,12 @@
+import { SessionProvider } from 'next-auth/react'
+
 import ToastProvider from './ToastProvider'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <SessionProvider>
       {children}
       <ToastProvider />
-    </>
+    </SessionProvider>
   )
 }
